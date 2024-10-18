322a21f08cd3c45aa8dd921e948ce5b21c9c1764
erikk4 <e.keplinger@students.htl-leonding.ac.at>
2024-10-18 10:00:48 +0200

    implemented task 1 - 3

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { AnswerQuestionComponent } from './answer-question/answer-question.component';
-import { SystemPromptComponent } from './system-prompt/system-prompt.component';
-
-export const routes: Routes = [
-  { path: 'answer-question', component: AnswerQuestionComponent },
-  { path: '', redirectTo: '/answer-question', pathMatch: 'full' },
-  { path: 'system-prompt', component: SystemPromptComponent },
-];
